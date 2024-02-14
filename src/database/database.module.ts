661eab0ef563d13e9acb23748774890b82cd822b
Eren Kurnaz <h.erenkurnaz@icloud.com>
2024-02-14 17:29:01 +0300

    fix: add missing entities to database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,10 +3,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { User, UserRepository } from './user';
 import { EntityManager } from '@mikro-orm/postgresql';
+import { Book, BookRepository } from './book';
+import { BookStore, BookStoreRepository } from './book-store';
 import { Role, RoleRepository } from './role';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User, Role])],
+  imports: [MikroOrmModule.forFeature([User, Role, Book, BookStore])],
   providers: [
     {
       provide: UserRepository,
@@ -18,7 +20,22 @@ import { Role, RoleRepository } from './role';
       useFactory: (em: EntityManager) => em.getRepository(Role),
       inject: [EntityManager],
     },
+    {
+      provide: BookRepository,
+      useFactory: (em: EntityManager) => em.getRepository(Book),
+      inject: [EntityManager],
+    },
+    {
+      provide: BookStoreRepository,
+      useFactory: (em: EntityManager) => em.getRepository(BookStore),
+      inject: [EntityManager],
+    },
   ],
-  exports: [UserRepository, RoleRepository],
+  exports: [
+    UserRepository,
+    RoleRepository,
+    BookRepository,
+    BookStoreRepository,
+  ],
 })
 export class DatabaseModule {}
