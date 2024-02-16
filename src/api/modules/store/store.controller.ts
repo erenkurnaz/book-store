7b2084ae6003b30038bdd6a4a82a13d6588c05da
Eren Kurnaz <h.erenkurnaz@icloud.com>
2024-02-16 19:36:58 +0300

    chore: use roles guard on book store controller

--- src/api/modules/store/store.controller.ts
+++ src/api/modules/store/store.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { Pagination, PaginationOptions } from '../../decorators';
 import { Store } from '../../../database/store';
+import { RolesGuard } from '../../../security/guards/roles.guard';
+import { Roles } from '../../decorators/roles.decorator';
+import { UserRole } from '../../../database/user';
 
 @Controller('store')
+@UseGuards(RolesGuard)
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
 
   @Get()
+  @Roles(UserRole.USER, UserRole.STORE_MANAGER, UserRole.ADMIN)
   public async getAll(
     @Pagination() pagination: PaginationOptions<Store>,
     @Query('keyword') keyword?: string,
